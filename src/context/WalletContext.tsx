710c9e873a970a6e726abf534a91780207871d9a
AllanTran90 <allan_sweden90@hotmail.com>
2026-05-13 10:07:45 +0200

    Connect wallet context to Supabase balance data

--- src/context/WalletContext.tsx
+++ src/context/WalletContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 type WalletContextType = {
   balance: number;
@@ -12,6 +13,27 @@ const WalletContext = createContext<WalletContextType | null>(null);
 export function WalletProvider({ children }: { children: React.ReactNode }) {
   const [balance, setBalance] = useState(150);
 
+    useEffect(() => {
+    async function fetchWallet() {
+      const { data, error } =
+        await supabase
+          .from("wallet")
+          .select("*")
+          .eq("id", 1)
+          .single();
+
+      if (data) {
+        setBalance(data.balance);
+      }
+
+      if (error) {
+        console.error(error);
+      }
+    }
+
+    fetchWallet();
+  }, []);
+
   return (
     <WalletContext.Provider value={{ balance, setBalance }}>
       {children}
@@ -21,6 +43,7 @@ export function WalletProvider({ children }: { children: React.ReactNode }) {
 
 export function useWallet() {
   const context = useContext(WalletContext);
+  
 
   if (!context) {
     throw new Error("useWallet must be used inside WalletProvider");
