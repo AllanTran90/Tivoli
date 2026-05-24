92676f1817d9c07e700ca8489b0b7fc9ced49cb6
AllanTran90 <allan_sweden90@hotmail.com>
2026-05-24 14:58:58 +0200

    feat: GameButton styling + InfoBar

--- src/components/Gamebutton.tsx
+++ src/components/Gamebutton.tsx
@@ -1,27 +1,22 @@
 "use client";
 
-type Props ={
-    text: string
-    disabled?: boolean
-    className?: string
-    onClick: () => void
+import styles from "./GameButton.module.css";
+
+type Props = {
+  text: string;
+  disabled?: boolean;
+  className?: string;
+  onClick: () => void;
 };
 
-export default function GameButton ({
-   text,
-   disabled,
-   onClick,
-   className
-}: 
-Props){
-
-    return(
-      <button
-        onClick={onClick}
-        disabled={disabled}
-        className={className}
-        >
-            {text}
-      </button>
-    );
+export default function GameButton({ text, disabled, onClick, className }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${className ?? ""}`}
+    >
+      {text}
+    </button>
+  );
 }
